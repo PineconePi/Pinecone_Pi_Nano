5b9a6651321721d08aed7c8e6d1994c475aaeac7
董程森|Chengsen Dong <1034029664@qq.com>
2019-01-10 19:42:04 +0800

    update
    update uart.c uart.h

---
+++ Library/system/UART/uart.h
@@ -0,0 +1,99 @@
+/**
+  ******************************************************************************
+  * @file    uart.h
+  * @author  PineconePi
+  * @version V1.0.0
+  * @date    20-December-2018
+  * @brief  This file is used to configure the serial port 
+  * @License:GNU General Public License v3.0         
+  ******************************************************************************
+  * @attention
+  *
+  *Serial Interrupt Processing Function in uart.c 
+  *  
+  * 
+  * 
+  * 
+  * 
+  *
+  * 
+  ******************************************************************************
+	**/
+#ifndef __UART_H
+#define __UART_H	 
+
+#include	"system.h"
+
+#define	COM_TX1_Lenth	128
+#define	COM_RX1_Lenth	128
+#define	COM_TX2_Lenth	128
+#define	COM_RX2_Lenth	128
+
+#define	USART1	1
+#define	USART2	2
+
+#define	UART_ShiftRight	0		//同步移位输出
+#define	UART_8bit_BRTx	(1<<6)	//8位数据,可变波特率
+#define	UART_9bit		(2<<6)	//9位数据,固定波特率
+#define	UART_9bit_BRTx	(3<<6)	//9位数据,可变波特率
+
+#define	UART1_SW_P30_P31	0
+#define	UART1_SW_P36_P37	(1<<6)
+#define	UART1_SW_P16_P17	(2<<6)	//必须使用内部时钟
+#define	UART2_SW_P10_P11	0
+#define	UART2_SW_P46_P47	1
+
+
+#define	TimeOutSet1		5
+#define	TimeOutSet2		5
+
+#define	BRT_Timer1	1
+#define	BRT_Timer2	2
+
+#define ENABLE  	1			//允许
+#define DISABLE  	0			//关闭
+
+#define PolityHigh  1	  //高优先级
+#define PolityLow  	0		//低优先级（默认）
+
+typedef struct
+{ 
+	unsigned char	id;				//串口号
+
+	unsigned char	TX_read;		//发送读指针
+	unsigned char	TX_write;		//发送写指针
+	unsigned char	B_TX_busy;		//忙标志
+
+	unsigned char 	RX_Cnt;			//接收字节计数
+	unsigned char	RX_TimeOut;		//接收超时
+	unsigned char	B_RX_OK;		//接收块完成
+} COMx_Define; 
+
+typedef struct
+{ 
+	unsigned char	UART_Mode;			//模式,         UART_ShiftRight,UART_8bit_BRTx,UART_9bit,UART_9bit_BRTx
+	unsigned char	UART_BRT_Use;		//使用波特率,   BRT_Timer1,BRT_Timer2
+	unsigned long	UART_BaudRate;		//波特率,       ENABLE,DISABLE
+	unsigned char	Morecommunicate;	//多机通讯允许, ENABLE,DISABLE
+	unsigned char	UART_RxEnable;		//允许接收,   ENABLE,DISABLE
+	unsigned char	BaudRateDouble;		//波特率加倍, ENABLE,DISABLE
+	unsigned char	UART_Interrupt;		//中断控制,   ENABLE,DISABLE
+	unsigned char	UART_Polity;		//优先级,     PolityLow,PolityHigh
+	unsigned char	UART_P_SW;			//切换端口,   UART1_SW_P30_P31,UART1_SW_P36_P37,UART1_SW_P16_P17(必须使用内部时钟)
+} COMx_InitDefine; 
+
+extern	COMx_Define	COM1,COM2;
+extern	unsigned char	xdata TX1_Buffer[COM_TX1_Lenth];	//发送缓冲
+extern	unsigned char 	xdata RX1_Buffer[COM_RX1_Lenth];	//接收缓冲
+extern	unsigned char	xdata TX2_Buffer[COM_TX2_Lenth];	//发送缓冲
+extern	unsigned char 	xdata RX2_Buffer[COM_RX2_Lenth];	//接收缓冲
+
+unsigned char	USART_Configuration(unsigned char UARTx, COMx_InitDefine *COMx);
+void TX1_write2buff(unsigned char dat);	//写入发送缓冲，指针+1
+void TX2_write2buff(unsigned char dat);	//写入发送缓冲，指针+1
+void PrintString1(unsigned char *puts);
+void PrintString2(unsigned char *puts);
+
+
+#endif
+
