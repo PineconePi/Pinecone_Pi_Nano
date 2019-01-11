5dbb3040a56e18bf90030db38acc12903d57ffa8
董程森|Chengsen Dong <1034029664@qq.com>
2019-01-11 13:40:43 +0800

    update
    update digitaltube.c digitaltube.h

---
+++ Library/device/digital tube/digitaltube.h
@@ -0,0 +1,36 @@
+/**
+  ******************************************************************************
+  * @file    digitaltube.h
+  * @author  PineconePi
+  * @version V1.0.0
+  * @date    20-December-2018
+  * @brief  This file is used to set up  digital tubes 
+  * @License:GNU General Public License v3.0         
+  ******************************************************************************
+  * @attention
+  *
+  *
+  *  
+  * 
+  * 
+  * 
+  * 
+  *
+  * 
+  ******************************************************************************
+	**/
+#ifndef _DIGITALTUBE_H_
+#define _DIGITALTUBE_H_
+
+#include "system.h"
+
+#define ENABLE 1  //使能
+#define DISABLE 0 //关闭
+
+#define SMG1 1  //数码管1
+#define SMG2 2  //数码管2
+
+
+void digitaltube_enable(unsigned char SMGx,unsigned char Enable_Disable);//使能数码管
+void digitaltube_display(unsigned char Decimal_point_Enable,unsigned char dat);//使能小数点，显示数字（0-9）
+#endif
