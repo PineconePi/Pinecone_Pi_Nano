cb8d26afc3e57eae93162a9c5c5be490e2c1f9ac
董程森|Chengsen Dong <1034029664@qq.com>
2019-01-20 10:58:06 +0800

    Update system.h

--- Library/system/SYSTEM(Important documents）/system.h
+++ Library/system/SYSTEM(Important documents）/system.h
@@ -25,11 +25,11 @@
 
 /*********************************************************/
 
-  #define MAIN_Fosc		24000000L	//定义主时钟24Mhz
-//#define MAIN_Fosc		22118400L	//定义主时钟
-//#define MAIN_Fosc		12000000L	//定义主时钟
-//#define MAIN_Fosc		11059200L	//定义主时钟
-//#define MAIN_Fosc		 5529600L	//定义主时钟
+  #define MAIN_Fosc		24000000UL	//定义主时钟24Mhz
+//#define MAIN_Fosc		22118400UL	//定义主时钟
+//#define MAIN_Fosc		12000000UL	//定义主时钟
+//#define MAIN_Fosc		11059200UL	//定义主时钟
+//#define MAIN_Fosc		 5529600UL	//定义主时钟
 
 
 /*********************************************************/
